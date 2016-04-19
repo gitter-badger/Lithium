e4f5a45c4606278109d83f01e52fa463cecf6ac9
Johannes Donath <johannesd@torchmind.com>
2016-04-19 12:55:57 +0200

    Updated implementation to fit in with the new base declarations.

--- communication/src/main/java/com/torchmind/lithium/communication/packet/HandshakePacket.java
+++ communication/src/main/java/com/torchmind/lithium/communication/packet/HandshakePacket.java
@@ -5,6 +5,7 @@ import com.torchmind.lithium.communication.storage.Version;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * <strong>Handshake Packet</strong>
@@ -13,16 +14,20 @@ import java.io.IOException;
  *
  * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
  */
-public class HandshakePacket implements Packet {
+public class HandshakePacket extends AbstractPacket {
         private final String fingerprint;
         private final Version version;
 
-        public HandshakePacket(@Nonnull Version version, @Nonnull String fingerprint) {
-                this.version = version;
+        public HandshakePacket(@Nonnull String fingerprint, @Nonnull Version version) {
+                super();
+
                 this.fingerprint = fingerprint;
+                this.version = version;
         }
 
-        public HandshakePacket(@Nonnull Buffer buffer) {
+        public HandshakePacket(@Nonnull UUID identifier, @Nonnull Buffer buffer) {
+                super(identifier);
+
                 this.version = buffer.readStorageValue(Version.class);
                 this.fingerprint = buffer.readString();
         }
